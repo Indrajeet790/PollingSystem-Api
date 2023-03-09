5185b7e08bbe51a27df87a5f71e50204c7489810
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-09 21:16:04 +0530

    create delete routes for deleteoption

--- routes/option.js
+++ routes/option.js
@@ -6,5 +6,6 @@ const optionController = require("../controller/optionController");
 router.post("/:id/create_option", optionController.createOption);
 
 router.get("/option/:id/add_vote", optionController.addVote);
+router.delete("/:id/delete", optionController.optionDelete);
 
 module.exports = router;
